dfe0cf63cb36eab957e44704a3969bb372b24676
Davide Mezzogori <dmezzogori@users.noreply.github.com>
2022-10-20 13:33:08 +0200

    Singleton metaclass now resettable

--- simulatte/utils/singleton.py
+++ simulatte/utils/singleton.py
@@ -5,3 +5,10 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+
+    def __getattr__(self, item):
+        return getattr(self._instances[self], item)
+
+    def clear(cls):
+        if cls in cls._instances:
+            cls._instances.pop(cls)
