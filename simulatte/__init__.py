2f4cc687e7c1ab218c41344b1b0a015957770639
Davide Mezzogori <dmezzogori@users.noreply.github.com>
2022-10-20 14:29:18 +0200

    Implement MonitoredResource

--- simulatte/__init__.py
+++ simulatte/__init__.py
@@ -1,3 +1,4 @@
 from . import utils
 from .job import Job
 from .workcenter import WorkCenter
+from .utils import Priority
--- simulatte/utils/__init__.py
+++ simulatte/utils/__init__.py
@@ -1,1 +1,2 @@
 from .singleton import Singleton
+from .priority import Priority
