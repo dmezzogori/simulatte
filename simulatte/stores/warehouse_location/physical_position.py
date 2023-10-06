d838b18ed0362b2d56a356b4c693581e5fa89192
Davide Mezzogori <dmezzogori@users.noreply.github.com>
2023-10-06 14:52:36 +0200

    Performance improvements to PhysicalPosition

--- simulatte/stores/warehouse_location/physical_position.py
+++ simulatte/stores/warehouse_location/physical_position.py
@@ -16,47 +16,40 @@ class PhysicalPosition:
     Represent the physical position within a WarehouseLocation.
     """
 
+    __slots__ = ("unit_load", "n_cases", "free", "busy")
+
     def __init__(self, unit_load: Pallet | None = None) -> None:
         self.unit_load = unit_load
-
-    @property
-    def n_cases(self) -> int:
-        """
-        Return the number of cases in the physical position.
-        """
-        if self.unit_load is not None:
-            return self.unit_load.n_cases
-        return 0
+        self.n_cases = 0 if unit_load is None else unit_load.n_cases
+        self.free = unit_load is None
+        self.busy = not self.free
 
     def put(self, *, unit_load: Pallet) -> None:
         """
         Load a unit load into the physical position.
         """
+
         if self.busy:
             raise PhysicalPositionBusy(self)
+
         self.unit_load = unit_load
+        self.n_cases = unit_load.n_cases
+        self.free = False
+        self.busy = True
 
     def get(self) -> Pallet:
         """
         Empty the physical position and return the unit load.
         Raises PhysicalPositionEmpty if the physical position is free.
         """
+
         if self.free:
             raise PhysicalPositionEmpty(self)
+
         unit_load = self.unit_load
         self.unit_load = None
+        self.n_cases = 0
+        self.free = True
+        self.busy = False
+
         return unit_load
-
-    @property
-    def free(self) -> bool:
-        """
-        Return True if no unit load is in the physical position.
-        """
-        return self.unit_load is None
-
-    @property
-    def busy(self) -> bool:
-        """
-        Return True if a unit load is in the physical position.
-        """
-        return not self.free
