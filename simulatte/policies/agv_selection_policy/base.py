e0d590d0824ffbb5e83f0cd6e8e3f173115de2d3
Davide Mezzogori <dmezzogori@users.noreply.github.com>
2023-10-06 14:50:33 +0200

    Added policies

--- simulatte/policies/agv_selection_policy/base.py
+++ simulatte/policies/agv_selection_policy/base.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from collections.abc import Collection
+    from collections.abc import Collection, Sequence
 
     from simulatte.agv.agv import AGV
 
@@ -11,7 +11,16 @@ if TYPE_CHECKING:
 class AGVSelectionPolicy:
     def __call__(self, *, agvs: Collection[AGV], exceptions: Collection[AGV] | None = None) -> AGV:
         """
-        Abstract method to select the best AGV for a given mission.
+        Abstract method to select the best AGV according to some logic.
+        """
+
+        raise NotImplementedError
+
+
+class MultiAGVSelectionPolicy:
+    def __call__(self, *, agvs: Collection[AGV], exceptions: Collection[AGV] | None = None) -> Sequence[AGV]:
+        """
+        Abstract method to select the best AGVs according to some logic.
         """
 
         raise NotImplementedError
